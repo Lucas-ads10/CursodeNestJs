ad01dcc3ab574806a5415dfaf1d30f79d2ea3908
Lucas-ads10 <lucasdssilva13@hotmail.com>
2025-02-21 01:05:23 -0300

    Read do Crud

--- Crud Api Reset/src/user/user.controller.ts
+++ Crud Api Reset/src/user/user.controller.ts
@@ -1,11 +1,21 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import {Controller, Post, Body, Get, Param } from "@nestjs/common";
 
 @Controller ('users')
 export class UserController{
 
 @Post()
 async create(@Body() body) {
-    return{body};
+    return {body};
 }
+
+@Get()
+async list() {
+    return{users:[]}
+}
+
+@Get(':id')
+    async show(@Param() params) {
+        return{user:{}, params}
+    }
 
 }
