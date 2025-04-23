6a417bf472bbb52b1bef57a1b2cb6d89d3adf874
Lucas-ads10 <lucasdssilva13@hotmail.com>
2025-04-23 00:51:00 -0300

    Realizando o upadate para o DB

--- Crud Api Reset/src/user/user.controller.ts
+++ Crud Api Reset/src/user/user.controller.ts
@@ -27,21 +27,13 @@ async list() {
     }
 
 @Put(':id')
-async update(@Body() {email, name, password}:UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number) {
-    return{
-        method: 'put',
-        email, name, password,
-        id  
-    }
+async update(@Body() data: UpdatePutUserDTO, @Param('id', ParseIntPipe) id: number) {
+    return this.userService.update(id, data);
 }
 
 @Patch(':id')
-async updatePartial(@Body() {email, name, password}:UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number) {
-    return{
-        method: 'put',
-        email, name, password,
-        id
-    }
+async updatePartial(@Body() data: UpdatePatchUserDTO, @Param('id', ParseIntPipe) id: number) {
+    return this.userService.updatePartial(id, data);
 }
 
 @Delete(':id')
